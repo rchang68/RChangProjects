c70f281ee3af8a08e00d521d9773fdf8d6cc727d
NotRyan195 <133409751+NotRyan195@users.noreply.github.com>
2025-04-24 08:39:30 -0400

    Add files via upload

---
+++ RChangTicTacToe-CPT-185/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace RChangTicTacToe_CPT_185
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.lblPos0 = new System.Windows.Forms.Label();
+			this.lblPos5 = new System.Windows.Forms.Label();
+			this.lblPos8 = new System.Windows.Forms.Label();
+			this.lblPos7 = new System.Windows.Forms.Label();
+			this.lblPos6 = new System.Windows.Forms.Label();
+			this.lblPos3 = new System.Windows.Forms.Label();
+			this.lblPos2 = new System.Windows.Forms.Label();
+			this.lblPos1 = new System.Windows.Forms.Label();
+			this.lblPos4 = new System.Windows.Forms.Label();
+			this.lblWinner = new System.Windows.Forms.Label();
+			this.btnNewGame = new System.Windows.Forms.Button();
+			this.btnExit = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// lblPos0
+			// 
+			this.lblPos0.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.lblPos0.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblPos0.Location = new System.Drawing.Point(83, 69);
+			this.lblPos0.Name = "lblPos0";
+			this.lblPos0.Size = new System.Drawing.Size(135, 123);
+			this.lblPos0.TabIndex = 0;
+			this.lblPos0.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.lblPos0.Click += new System.EventHandler(this.lblPos0_Click_1);
+			// 
+			// lblPos5
+			// 
+			this.lblPos5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.lblPos5.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblPos5.Location = new System.Drawing.Point(418, 223);
+			this.lblPos5.Name = "lblPos5";
+			this.lblPos5.Size = new System.Drawing.Size(135, 123);
+			this.lblPos5.TabIndex = 1;
+			this.lblPos5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.lblPos5.Click += new System.EventHandler(this.lblPos5_Click_1);
+			// 
+			// lblPos8
+			// 
+			this.lblPos8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.lblPos8.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblPos8.Location = new System.Drawing.Point(418, 374);
+			this.lblPos8.Name = "lblPos8";
+			this.lblPos8.Size = new System.Drawing.Size(135, 123);
+			this.lblPos8.TabIndex = 2;
+			this.lblPos8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.lblPos8.Click += new System.EventHandler(this.lblPos8_Click_1);
+			// 
+			// lblPos7
+			// 
+			this.lblPos7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.lblPos7.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblPos7.Location = new System.Drawing.Point(252, 374);
+			this.lblPos7.Name = "lblPos7";
+			this.lblPos7.Size = new System.Drawing.Size(135, 123);
+			this.lblPos7.TabIndex = 3;
+			this.lblPos7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.lblPos7.Click += new System.EventHandler(this.lblPos7_Click_1);
+			// 
+			// lblPos6
+			// 
+			this.lblPos6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.lblPos6.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblPos6.Location = new System.Drawing.Point(83, 372);
+			this.lblPos6.Name = "lblPos6";
+			this.lblPos6.Size = new System.Drawing.Size(135, 123);
+			this.lblPos6.TabIndex = 4;
+			this.lblPos6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.lblPos6.Click += new System.EventHandler(this.lblPos6_Click_1);
+			// 
+			// lblPos3
+			// 
+			this.lblPos3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.lblPos3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblPos3.Location = new System.Drawing.Point(83, 223);
+			this.lblPos3.Name = "lblPos3";
+			this.lblPos3.Size = new System.Drawing.Size(135, 123);
+			this.lblPos3.TabIndex = 5;
+			this.lblPos3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.lblPos3.Click += new System.EventHandler(this.lblPos3_Click);
+			// 
+			// lblPos2
+			// 
+			this.lblPos2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.lblPos2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblPos2.Location = new System.Drawing.Point(418, 69);
+			this.lblPos2.Name = "lblPos2";
+			this.lblPos2.Size = new System.Drawing.Size(135, 123);
+			this.lblPos2.TabIndex = 6;
+			this.lblPos2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.lblPos2.Click += new System.EventHandler(this.lblPos2_Click);
+			// 
+			// lblPos1
+			// 
+			this.lblPos1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.lblPos1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblPos1.Location = new System.Drawing.Point(252, 69);
+			this.lblPos1.Name = "lblPos1";
+			this.lblPos1.Size = new System.Drawing.Size(135, 123);
+			this.lblPos1.TabIndex = 7;
+			this.lblPos1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.lblPos1.Click += new System.EventHandler(this.lblPos1_Click);
+			// 
+			// lblPos4
+			// 
+			this.lblPos4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.lblPos4.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblPos4.Location = new System.Drawing.Point(252, 223);
+			this.lblPos4.Name = "lblPos4";
+			this.lblPos4.Size = new System.Drawing.Size(135, 123);
+			this.lblPos4.TabIndex = 8;
+			this.lblPos4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.lblPos4.Click += new System.EventHandler(this.lblPos4_Click_1);
+			// 
+			// lblWinner
+			// 
+			this.lblWinner.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.lblWinner.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblWinner.Location = new System.Drawing.Point(181, 522);
+			this.lblWinner.Name = "lblWinner";
+			this.lblWinner.Size = new System.Drawing.Size(271, 46);
+			this.lblWinner.TabIndex = 9;
+			this.lblWinner.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.lblWinner.Click += new System.EventHandler(this.lblWinner_Click);
+			// 
+			// btnNewGame
+			// 
+			this.btnNewGame.Location = new System.Drawing.Point(124, 595);
+			this.btnNewGame.Name = "btnNewGame";
+			this.btnNewGame.Size = new System.Drawing.Size(148, 81);
+			this.btnNewGame.TabIndex = 10;
+			this.btnNewGame.Text = "Begin Game";
+			this.btnNewGame.UseVisualStyleBackColor = true;
+			this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click_1);
+			// 
+			// btnExit
+			// 
+			this.btnExit.Location = new System.Drawing.Point(366, 595);
+			this.btnExit.Name = "btnExit";
+			this.btnExit.Size = new System.Drawing.Size(148, 81);
+			this.btnExit.TabIndex = 11;
+			this.btnExit.Text = "E&xit";
+			this.btnExit.UseVisualStyleBackColor = true;
+			this.btnExit.Click += new System.EventHandler(this.btnExit_Click_1);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(638, 702);
+			this.Controls.Add(this.btnExit);
+			this.Controls.Add(this.btnNewGame);
+			this.Controls.Add(this.lblWinner);
+			this.Controls.Add(this.lblPos4);
+			this.Controls.Add(this.lblPos1);
+			this.Controls.Add(this.lblPos2);
+			this.Controls.Add(this.lblPos3);
+			this.Controls.Add(this.lblPos6);
+			this.Controls.Add(this.lblPos7);
+			this.Controls.Add(this.lblPos8);
+			this.Controls.Add(this.lblPos5);
+			this.Controls.Add(this.lblPos0);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label lblPos0;
+		private System.Windows.Forms.Label lblPos5;
+		private System.Windows.Forms.Label lblPos8;
+		private System.Windows.Forms.Label lblPos7;
+		private System.Windows.Forms.Label lblPos6;
+		private System.Windows.Forms.Label lblPos3;
+		private System.Windows.Forms.Label lblPos2;
+		private System.Windows.Forms.Label lblPos1;
+		private System.Windows.Forms.Label lblPos4;
+		private System.Windows.Forms.Label lblWinner;
+		private System.Windows.Forms.Button btnNewGame;
+		private System.Windows.Forms.Button btnExit;
+	}
+}
+
---
+++ RChangTicTacToe-CPT-185/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RChangTicTacToe_CPT_185
+{
+	public partial class Form1 : Form
+	{
+		int[,] TicTacToeBox = new int[3, 3];
+		int player1 = 1;
+		int player2 = 0;
+		int turn = 0;
+		bool ready = false;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+		private void lblPos0_Click_1(object sender, EventArgs e)
+		{
+			if (turn % 2 == 0)
+			{
+				lblPos0.Text = "X";
+				//Position 0 is 0,0
+				play(0, 0, player1);
+
+			}
+			else
+			{
+				//player 2 turn
+				lblPos0.Text = "O";
+				play(0, 0, player2);
+			}
+			turn++;
+
+		}
+
+		private void play(int row, int column, int player)
+		{
+			TicTacToeBox[row, column] = player;
+			checkWinner(player);
+		}
+
+		private void checkWinner(int player)
+		{
+			//horizontal, vertical
+
+			for (int i = 0; i < TicTacToeBox.GetLength(0); i++)
+			{
+				if ((TicTacToeBox[i, 0] == player && TicTacToeBox[i, 1] == player && TicTacToeBox[i, 1] == player && TicTacToeBox[i, 2] == player)
+					|| (TicTacToeBox[0, i] == TicTacToeBox[1, i] && TicTacToeBox[1, i] == player && TicTacToeBox[2, i] == player))
+				{
+					setWinner(player);
+				}
+
+
+				if ((TicTacToeBox[i, 0] == player && TicTacToeBox[i, 1] == player && TicTacToeBox[i, 2] == player) ||
+					(TicTacToeBox[0, i] == player && TicTacToeBox[1, i] == player && TicTacToeBox[2, i] == player))
+				{
+					setWinner(player);
+				}
+				//diagonal compare
+				if ((TicTacToeBox[0, 0] == player && TicTacToeBox[1, 1] == player && TicTacToeBox[1, 1] == player && TicTacToeBox[2, 2] == player)
+					|| (TicTacToeBox[0, 2] == player && TicTacToeBox[1, 1] == player && TicTacToeBox[1, 1] == player && TicTacToeBox[2, 0] == player))
+				{
+					setWinner(player);
+					return;
+				}
+				if (turn == 8)
+				{
+					lblWinner.Text = "It's A Tie!!!";
+				}
+			}
+		}
+
+		private void setWinner(int player)
+		{
+			if (player == 1)
+				lblWinner.Text = "Player 1 Wins. X's Rule";
+			else
+				lblWinner.Text = "Player 2 Wins. O's Rule";
+		}
+
+		private void lblPos1_Click(object sender, EventArgs e)
+		{
+			if (turn % 2 == 0)
+			{
+				lblPos1.Text = "X";
+				//Position 0 is 0,0
+				play(0, 1, player1);
+
+			}
+			else
+			{
+				//player 2 turn
+				lblPos1.Text = "O";
+				play(0, 1, player2);
+			}
+			turn++;
+		}
+
+		private void lblPos2_Click(object sender, EventArgs e)
+		{
+			if (turn % 2 == 0)
+			{
+				lblPos2.Text = "X";
+				//Position 0 is 0,0
+				play(0, 2, player1);
+
+			}
+			else
+			{
+				//player 2 turn
+				lblPos2.Text = "O";
+				play(0, 2, player2);
+			}
+			turn++;
+		}
+
+		private void lblPos3_Click(object sender, EventArgs e)
+		{
+			if (turn % 2 == 0)
+			{
+				lblPos3.Text = "X";
+				//Position 0 is 0,0
+				play(1, 0, player1);
+
+			}
+			else
+			{
+				//player 2 turn
+				lblPos3.Text = "O";
+				play(1, 0, player2);
+			}
+			turn++;
+		}
+
+		private void lblPos4_Click_1(object sender, EventArgs e)
+		{
+			if (turn % 2 == 0)
+			{
+				lblPos4.Text = "X";
+				//Position 0 is 0,0
+				play(1, 1, player1);
+
+			}
+			else
+			{
+				//player 2 turn
+				lblPos4.Text = "O";
+				play(1, 1, player2);
+			}
+			turn++;
+		}
+
+		private void lblPos5_Click_1(object sender, EventArgs e)
+		{
+			if (turn % 2 == 0)
+			{
+				lblPos5.Text = "X";
+				//Position 0 is 0,0
+				play(1, 2, player1);
+
+			}
+			else
+			{
+				//player 2 turn
+				lblPos5.Text = "O";
+				play(1, 2, player2);
+			}
+			turn++;
+		}
+
+		private void lblPos6_Click_1(object sender, EventArgs e)
+		{
+			if (turn % 2 == 0)
+			{
+				lblPos6.Text = "X";
+				//Position 0 is 0,0
+				play(2, 0, player1);
+
+			}
+			else
+			{
+				//player 2 turn
+				lblPos6.Text = "O";
+				play(2, 0, player2);
+			}
+			turn++;
+		}
+
+		private void lblPos7_Click_1(object sender, EventArgs e)
+		{
+			if (turn % 2 == 0)
+			{
+				lblPos7.Text = "X";
+				//Position 0 is 0,0
+				play(2, 1, player1);
+
+			}
+			else
+			{
+				//player 2 turn
+				lblPos7.Text = "O";
+				play(2, 1, player2);
+			}
+			turn++;
+		}
+
+		private void lblPos8_Click_1(object sender, EventArgs e)
+		{
+			if (turn % 2 == 0)
+			{
+				lblPos8.Text = "X";
+				//Position 0 is 0,0
+				play(2, 2, player1);
+
+			}
+			else
+			{
+				//player 2 turn
+				lblPos8.Text = "O";
+				play(2, 2, player2);
+			}
+			turn++;
+		}
+
+		private void btnNewGame_Click_1(object sender, EventArgs e)
+		{
+			//Start Game
+			//Set Round to Zero
+			//build our tic tac toe gridusing a 2d 
+			for (int row = 0; row < TicTacToeBox.GetLength(0); row++)
+			{
+				for (int col = 0; col < TicTacToeBox.GetLength(1); col++)
+				{
+					//setting tic tac toe box to -1: nothing is entered
+					TicTacToeBox[row, col] = -1;
+				}
+			}
+			lblPos0.Text = "";
+			lblPos1.Text = "";
+			lblPos2.Text = "";
+			lblPos3.Text = "";
+			lblPos4.Text = "";
+			lblPos5.Text = "";
+			lblPos6.Text = "";
+			lblPos7.Text = "";
+			lblPos8.Text = "";
+			lblWinner.Text = "";
+			MessageBox.Show("Game Ready to Begin");
+			ready = true;
+
+		}
+
+		private void btnExit_Click_1(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		
+
+		private void lblWinner_Click(object sender, EventArgs e)
+		{
+
+		}
+
+	}
+}
